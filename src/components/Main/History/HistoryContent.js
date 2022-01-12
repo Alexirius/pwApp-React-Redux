bd5424943bfa00a53bac2ffa8e09123ebc303b49
industrial-marketing <49843245+industrial-marketing@users.noreply.github.com>
2022-01-12 20:08:47 +0600

    filteredArray() is wrapped with useMemo

--- src/components/Main/History/HistoryContent.js
+++ src/components/Main/History/HistoryContent.js
@@ -1,23 +1,6 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 
 const HistoryContent = ({historyArray, filterString, filterFlag}) => {
-
-    const filterArray = (arr) => {
-		if (filterString.length) {          //filter table by any field values
-			arr = arr.filter((curObj) => {
-				return curObj.date.toUpperCase().includes(filterString.toUpperCase()) ||
-					curObj.username.toUpperCase().includes(filterString.toUpperCase()) ||
-					curObj.amount.toString().includes(filterString) ||
-					curObj.balance.toString().includes(filterString)
-			})
-		}
-		if (filterFlag === 'debit') {           // filter debit/credit
-			arr = arr.filter(({amount}) => amount > 0)
-		} else if (filterFlag === 'credit') {
-			arr = arr.filter(({amount}) => amount < 0)
-		}
-		return arr;
-	}
 
     const renderTable = (arr) => {
 		return arr.map( (item) => {
@@ -33,11 +16,31 @@ const HistoryContent = ({historyArray, filterString, filterFlag}) => {
 		});
 	}
 
-    if (!historyArray.length) {
+	const filteredArray = useMemo ( () => {
+		const filterArray = (arr) => {
+			// console.log('filterArray');
+			if (filterString.length) {          //filter table by any field values
+				arr = arr.filter((curObj) => {
+					return curObj.date.toUpperCase().includes(filterString.toUpperCase()) ||
+						curObj.username.toUpperCase().includes(filterString.toUpperCase()) ||
+						curObj.amount.toString().includes(filterString) ||
+						curObj.balance.toString().includes(filterString)
+				})
+			}
+			if (filterFlag === 'debit') {           // filter debit/credit
+				arr = arr.filter(({amount}) => amount > 0)
+			} else if (filterFlag === 'credit') {
+				arr = arr.filter(({amount}) => amount < 0)
+			}
+			return arr;
+		}
+		return filterArray(historyArray);
+	},[historyArray, filterString, filterFlag]);
+    
+	if (!historyArray.length) {
         return 'You have no Transactions History yet.';
     }
-	historyArray = filterArray(historyArray);
-    if (!historyArray.length) {
+    if (!filteredArray.length) {
         return 'No matches found.';
     }
     const tHead = (
@@ -55,7 +58,7 @@ const HistoryContent = ({historyArray, filterString, filterFlag}) => {
         <table className="history-table">
             {tHead}
             <tbody>
-                {renderTable(historyArray)}
+                {renderTable(filteredArray)}
             </tbody>
         </table>
     );
